39898f78d6debb559015842e2338ab09baa9daa5
amalalm <amalmoh.mtr@gmail.com>
2024-09-26 17:05:30 +0300

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Amal almutairi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
